bd37ef1ffb468c0c6a4f8db4b720a26bee82d799
GabrielHorbach <gabrielhorbachmachado@hotmail.com>
2020-03-04 17:39:10 -0300

    feat: implements styled-components global style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 import Routes from '~/routes';
+import GlobalStyle from '~/globalStyle';
 
 export default function App() {
   return (
-    <Routes />
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
   );
 }
